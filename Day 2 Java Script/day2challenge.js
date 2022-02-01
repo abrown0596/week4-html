604d2ab81c166455cd43ebce3be38ccf81676b36
Adam Brown <abrown0596@hotmail.co.uk>
2022-02-01 16:07:50 +0000

    updated bonus

--- Day 2 Java Script/day2challenge.js
+++ Day 2 Java Script/day2challenge.js
@@ -27,18 +27,6 @@
 
 
 //BONUS CHALLENGE:
-
-// let i = 1;
-// while (i < 101){
-//     if (i%15==0){
-//         console.log("FizzBuzz")
-//     } else if (i%3==0){
-//         console.log("Fizz")
-//     } else if(i%5==0){
-//         console.log("Buzz")
-//     } else {console.log(i)}
-//     i++
-// };
 // for(let i=1;i<101;i++){if(i%15==0){console.log("FizzBuzz")}else if (i%3==0){console.log("Fizz")}else if(i%5==0){console.log("Buzz")}else {console.log(i)}};
 
 //Code I definitely didn't google for Bonus challenge:
