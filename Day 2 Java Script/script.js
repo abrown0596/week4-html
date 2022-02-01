1a052b98e5a2490b37de3d64018c9cba91b1690d
Adam Brown <abrown0596@hotmail.co.uk>
2022-02-01 10:34:24 +0000

    added first exercise

---
+++ Day 2 Java Script/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let name = "Adam";
+let surname="Brown";
+let home="Mansfield";
+let realscience="Taurus";
+console.log("First Exercise");
+
+console.log(name);
+console.log(surname);
+console.log(home);
+console.log(realscience);
+
+
+console.log("Third Exercise");
+
+console.log("%c" +"This is my example text with CSS properties", "color: orange; font:fantasy; font-style: bold; background-color: black; padding: 10px");
+
+
+
