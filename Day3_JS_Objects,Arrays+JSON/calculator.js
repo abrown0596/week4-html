01b4d1f8e58fecf6b270a32e7b4a84a976131ae2
Adam Brown <abrown0596@hotmail.co.uk>
2022-02-03 11:01:20 +0000

    edited calc

--- Day3_JS_Objects,Arrays+JSON/calculator.js
+++ Day3_JS_Objects,Arrays+JSON/calculator.js
@@ -10,6 +10,7 @@ DOM.addButton.onclick = () => addition(DOM.input1.value,DOM.input2.value);
 DOM.subtractButton.onclick = () => subtraction(DOM.input1.value,DOM.input2.value);
 DOM.multiplyButton.onclick = () => multiplication(DOM.input1.value,DOM.input2.value);
 DOM.divideButton.onclick = () => division(DOM.input1.value,DOM.input2.value);
+DOM.equalsbutton.onclick = () => 
 
 function addToList(str) {
 
@@ -19,25 +20,23 @@ function addToList(str) {
     DOM.list.appendChild(line);
 }
 
+
 function addition(a,b){
     let result = Number(a) + Number(b);
-    addToList(result);
+    addToList(`${a} + ${b} = ` + result);
 }
 
 function subtraction(a,b){
     let result = Number(a) - Number(b);
-    addToList(result);
+    addToList(`${a} - ${b} = ` + result);
 }
 
 function multiplication(a,b){
     let result = Number(a) * Number(b);
-    addToList(result);
+    addToList(`${a} * ${b} = ` + result);
 }
 
 function division(a,b){
     let result = Number(a) / Number(b);
-    addToList(result);
+    addToList(`${a} / ${b} = ` + result);
 }
-
-
-
